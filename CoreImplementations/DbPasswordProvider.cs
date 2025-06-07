69c4ed29c6b51b0d6841a4f876c8aa59f0b15f96
danil54543 <danil54543@gmail.com>
2025-06-07 18:00:56 +0300

    implement DbPasswordProvider

--- CoreImplementations/DbPasswordProvider.cs
+++ CoreImplementations/DbPasswordProvider.cs
@@ -1,14 +1,34 @@
 ﻿using ArqanumCore.Interfaces;
+using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace Arqanum.CoreImplementations
 {
     internal class DbPasswordProvider : IDbPasswordProvider
     {
-        public Task<string> GetDatabasePassword()
+        private const string FileName = "dbkey_protected.bin";
+
+        public async Task<string> GetDatabasePassword()
         {
-            // Placeholder implementation, replace with actual logic to retrieve the database password
-            return Task.FromResult("YourDatabasePasswordHere");
+            var path = Path.Combine(
+           Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+           FileName);
+
+            if (File.Exists(path))
+            {
+                var protectedData = File.ReadAllBytes(path);
+                var unprotected = ProtectedData.Unprotect(protectedData, null, DataProtectionScope.CurrentUser);
+                return Convert.ToBase64String(unprotected);
+            }
+            else
+            {
+                var rawPassword = RandomNumberGenerator.GetBytes(32);
+                var protectedData = ProtectedData.Protect(rawPassword, null, DataProtectionScope.CurrentUser);
+                File.WriteAllBytes(path, protectedData);
+                return Convert.ToBase64String(rawPassword);
+            }
         }
     }
 }
